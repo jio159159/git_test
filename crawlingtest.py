30ce0e6023364edc78e3ea6e8135a8890c04fc83
jio159159 <jio159159@naver.com>
2021-01-24 14:34:35 +0900

    test

--- crawlingtest.py
+++ crawlingtest.py
@@ -8,12 +8,20 @@ def get_html(url): # requests모듈 공식 홈페이지에 나와있는 내용
     if resp.status_code == 200:
         _html = resp.text
     return _html
-
+    
+number = '010-4919-2952'
 URL = "http://www.missed-call.com/"
 html = get_html(URL) 
 soup = BeautifulSoup(html, 'html.parser')
 Spam_Lookup = soup.find("table", attrs={"class": "CB_Table"})
-print(Spam_Lookup)
+
+session=requests.session()
+res=session.post(URL, data = number)
+res.raise_for_status()
+print(res.headers)
+
+
+#print(Spam_Lookup)
 from html_table_parser import parser_functions as parser
 html_table = parser.make2d(Spam_Lookup)
 #print(html_table)
